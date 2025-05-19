16184d9dd040df9141449de3fc7770eaef1a881f
restumardiko <mardikorestu@gmail.com>
2025-05-19 19:15:36 +0700

    upgate

--- component/controlledForm.jsx
+++ component/controlledForm.jsx
@@ -7,16 +7,17 @@ function Form({ handle }) {
   const handleChange = (e) => {
     e.preventDefault();
     const value = e.target.value;
+    console.log(isi);
     setIsi(value);
   };
   const handleClick = (e) => {
     e.preventDefault();
-    handle(isi);
+    handle(isi.trim());
   };
   return (
     <div className="form">
       <div>
-        <h1> Lemme forecast your location !</h1>
+        <h1 className="offering-text"> Lemme forecast your location !</h1>
         <form onSubmit={handleClick}>
           <input id="name" type="text" required onChange={handleChange} />
           <br />
