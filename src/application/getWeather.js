e4d21c19fa6dbdcb9c1868c9e5d42251a6baa6f3
restumardiko <mardikorestu@gmail.com>
2026-04-21 14:59:33 +0700

    add useCase

---
+++ src/application/getWeather.js
@@ -0,0 +1,7 @@
+export default async function getWeatherUseCase(city,){
+  //ambil data dari api
+  const data = await 
+  //tentukan kondisi weather
+  //tentukan background
+  //update data ke UI
+}
---
+++ src/domain/weatherValue.js
@@ -0,0 +1,20 @@
+export default function weatherValue(weatherData) {
+  //this contain value from raw data
+  const city = weatherData.name;
+  const condition = weatherData.weather[0].main;
+  const temperature = weatherData.main.temp;
+  const humidity = weatherData.main.humidity;
+  const pressure = weatherData.main.pressure;
+  const windSpeed = weatherData.wind.speed;
+  const visibility = weatherData.visibility;
+
+  return {
+    city: city,
+    condition: condition,
+    temperature: temperature,
+    humidity: humidity,
+    pressure: pressure,
+    windSpeed: windSpeed,
+    visibility: visibility,
+  };
+}
