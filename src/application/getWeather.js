0797a784501ebfb853163af3f63999a7c90a105d
restumardiko <mardikorestu@gmail.com>
2026-06-25 22:32:53 +0700

    cleaning code by swapping bad smell code

--- src/application/getWeather.js
+++ src/application/getWeather.js
@@ -4,17 +4,15 @@ import getWeatherCondition from "../domain/weatherCondition";
 import getWeatherValue from "../domain/weatherValue";
 
 export default async function getWeatherUseCase(city, getWeatherRepo) {
-  //ambil data dari api
   const data = await getWeatherRepo(city);
-  //mapper
+
   const mappedData = mappingTheWeatherRepo(data);
 
-  //tentukan weather value
   const weatherValue = getWeatherValue(mappedData);
-  //tentukan kondisi weather
+
   const condition = getWeatherCondition(mappedData);
-  //tentukan background
+
   const background = getBackground(condition);
-  //update data ke UI
+
   return { weatherValue, background };
 }
--- src/domain/background.js
+++ src/domain/background.js
@@ -1,20 +1,3 @@
 export default function getBackground(condition) {
-  // if (condition == "Rain") {
-  //   return "rain.webp";
-  // }
-  // if (condition == "Haze") {
-  //   return "haze.webp";
-  // }
-  // if (condition == "Clouds") {
-  //   return "clouds.webp";
-  // }
-  // if (condition == "Mist") {
-  //   return "mist.webp";
-  // }
-  // if (condition == "Clear") {
-  //   return "clear.webp";
-  // }
-  // return "default.webp";
-
   return condition;
 }
